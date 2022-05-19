ef026c2844538e7e5ff4a3cc0e92f370b766b622
JorgeCal123/boom.git <git@github.com>
2022-05-19 10:14:05 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-
 """script that, using this REST API, for a given employee ID"""
 import requests
 from sys import argv
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    user_dict = user.json()
 
-user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
-user_dict = user.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos_dict = todos.json()
+    done_task = 0
+    all_task = 0
+    tasks = ""
+    for t in todos_dict:
+        if t['userId'] == int(argv[1]):
+            all_task += 1
+            if t['completed']:
+                done_task += 1
+                tasks += '\t' + t['title'] + '\n'
 
-todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-todos_dict = todos.json()
-done_task = 0
-all_task = 0
-tasks = ""
-for t in todos_dict:
-    if t['userId'] == int(argv[1]):
-        all_task += 1
-        if t['completed']:
-            done_task += 1
-            tasks += '\t' + t['title'] + '\n'
-
-msm = "Employee {} is done with tasks({}/{}):\n{}".format(
-        user_dict['name'], done_task, all_task, tasks)
-print(msm, end="")
+    msm = "Employee {} is done with tasks({}/{}):\n{}".format(
+            user_dict['name'], done_task, all_task, tasks)
+    print(msm, end="")
