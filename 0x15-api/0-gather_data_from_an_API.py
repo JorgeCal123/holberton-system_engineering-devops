da7ff4036a58973ed76c7fd711d9cfa84ba0e84a
JorgeCal123/boom.git <git@github.com>
2022-05-19 10:18:07 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,8 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-    user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1])
     user_dict = user.json()
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
@@ -16,7 +17,7 @@ if __name__ == "__main__":
             all_task += 1
             if t['completed']:
                 done_task += 1
-                tasks += '\t' + t['title'] + '\n'
+                tasks += "\t{}\n".format(t['title'])
 
     msm = "Employee {} is done with tasks({}/{}):\n{}".format(
             user_dict['name'], done_task, all_task, tasks)
