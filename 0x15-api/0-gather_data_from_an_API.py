0ee7841fdfe23319542194403dd6a611a9dd3da6
JorgeCal123/boom.git <git@github.com>
2022-05-19 10:02:12 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
+import requests
 from sys import argv
-import  json
 
-url = 'https://jsonplaceholder.typicode.com/todos/'
-with urlopen(url + argv[1]) as response:
-    body = response.read()
-todo_item = body.json()
-print (todo_item)
+
+user = requests.get('https://jsonplaceholder.typicode.com/users/'+ argv[1])
+user_dict = user.json()
+
+todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos_dict = todos.json()
+done_task = 0
+all_task = 0
+tasks = ""
+for t in todos_dict:
+    if t['userId'] == int(argv[1]):
+        all_task += 1
+        if t['completed']:
+            done_task += 1
+            tasks += '\t' + t['title'] + '\n'
+
+msm = "Employee {} is done with tasks({}/{}):\n{}".format(user_dict['name'], done_task, all_task, tasks)
+print(msm, end="")
