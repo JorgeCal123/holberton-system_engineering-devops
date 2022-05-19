d31570f9701b1f13e3772f2c09b2d2720532889a
JorgeCal123/boom.git <git@github.com>
2022-05-19 12:56:24 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,30 +6,28 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                        argv[1])
-    user_dict = user.json()
 
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1]
+                        )
+    user_dic = user.json()
+    user = user_dic["username"]
+
+    taskarr = []
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos_dict = todos.json()
-
-    dict_arr = []
-    labels = ['userId', 'username', 'completed', 'title']
-
-    for t in todos_dict:
-        if t['userId'] == int(argv[1]):
-            dict_format = {}
-            dict_format['userId'] = t['userId']
-            dict_format['username'] = user_dict['username']
-            dict_format['completed'] = t['completed']
-            dict_format['title'] = t['title']
-            dict_arr.append(dict_format)
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            task = {}
+            task["user_id"] = t["userId"]
+            task["username"] = user
+            task["completed"] = t["completed"]
+            task["title"] = t["title"]
+            taskarr.append(task)
 
     try:
-        name_file = argv[1] + ".csv"
-        with open(name_file, 'w') as f:
+        id = "{}.csv".format(argv[1])
+        with open(id, 'w') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for elem in dict_arr:
+            for elem in taskarr:
                 writer.writerow(elem.values())
     except IOError:
         print("I/O error")
