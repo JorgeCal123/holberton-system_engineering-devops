0023bf6203d2e41b1c7c2b39656384a957e0752a
JorgeCal123/boom.git <git@github.com>
2022-05-19 12:14:43 -0700

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1])
+    user_dict = user.json()
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos_dict = todos.json()
+
+    
+    dict_arr = []
+    labels = ['userId', 'username', 'completed', 'title']
+
+    for t in todos_dict:
+        if t['userId'] == int(argv[1]):
+            dict_format = {}
+            dict_format['userId'] = t['userId']
+            dict_format['username'] = user_dict['username']
+            dict_format['completed'] = t['completed']
+            dict_format['title'] = t['title']
+            dict_arr.append(dict_format)
+            #print(dict_format)
+    try:
+        name_file = argv[1] + ".csv"
+        with open(name_file, 'w') as f:
+            writer = csv.DictWriter(f, quoting=csv.QUOTE_ALL, fieldnames=labels)
+            writer.writeheader()
+            for elem in dict_arr:
+                writer.writerow(elem)
+    except IOError:
+        print("I/O error")
